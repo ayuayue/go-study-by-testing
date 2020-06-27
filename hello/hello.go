8373748db34df4d399ceedad107babb5fcbab133
ayuayue <1401262639@qq.com>
2020-06-27 20:54:01 +0800

    first testing

---
+++ hello/hello.go
@@ -0,0 +1,17 @@
+package main
+
+const englishHelloPrefix = "Hello, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+
+}
---
+++ hello/hello_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+	t.Run("saying Hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+}
